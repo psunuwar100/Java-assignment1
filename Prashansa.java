fcffdf2cbf6ba5e06f6a54c3bfb08d2b01f3d647
psunuwar100 <psunuwar100@gmail.com>
2023-02-13 17:48:14 +0545

    assignment 1

---
+++ Prashansa.java
@@ -0,0 +1,12 @@
+public class Prashansa{
+    public static void main(String[] args){
+        System.out.println("Name of the organization: Prashansa ko organization");
+        System.out.println("Address:");
+        System.out.println("---State:Bagmati");
+        System.out.println("---ZipCode:0000");
+        System.out.println("---Country:Nepal");
+        System.out.println("Contact Information:");
+        System.out.println("---Mobile number:9841122096");
+        System.out.println("---Email:prashansadada@gmail.com");
+    }
+}
